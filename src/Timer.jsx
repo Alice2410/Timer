86d3fb03baddb2eba7541a021c5512ce60b6a2c7
Alice2410 <devo4ka2410@gmail.com>
2023-05-15 19:20:25 +0300

    feat: add seconds counter

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,5 +1,16 @@
+import { useState, useEffect } from 'react';
 
 const Timer = () => {
+  const [timeCounter, setTimeCounter] = useState(0);
+
+  useEffect(() => {
+    const timeIntervalId = setInterval(() => setTimeCounter((counter) => counter + 1), 1000)
+
+    return () => {
+      clearInterval(timeIntervalId)
+    };
+  },[])
+ 
   return(
     <div>Initialized</div>
   )
