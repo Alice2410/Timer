03c77403a4ed157edc51722809ffa84c36d9fe7d
Alice2410 <devo4ka2410@gmail.com>
2023-05-15 19:23:17 +0300

    feat: get time string in hh:mm:ss format

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -15,8 +15,17 @@ const Timer = () => {
     return time < 10 ? `0${time}` : time;
   }
 
+  const getTime = () => {
+    const seconds = timeCounter % 60;
+    const totalMinutes = (timeCounter - seconds) / 60;
+    const minutes = totalMinutes % 60;
+    const hours = (totalMinutes - minutes) / 60 % 100;
+
+    return `${getFormatedTimeString(hours)}:${getFormatedTimeString(minutes)}:${getFormatedTimeString(seconds)}`
+  }
+
   return(
-    <div>Initialized</div>
+    <div>{getTime()}</div>
   )
 };
 
